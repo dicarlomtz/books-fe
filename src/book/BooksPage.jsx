502e856b5a919ffedb3fbbe6658c8d8c20079631
dicarlomtz <dicarlomtz@gmail.com>
2022-09-17 14:54:59 -0600

    Adding fonts and icons

--- src/book/BooksPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const BooksPage = () => {
-  return (
-    <div>BooksPage</div>
-  )
-}
---
+++ src/book/pages/BooksPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useSearchBooks } from '../../hooks/book/useSearchBooks'
+
+export const BooksPage = () => {
+  const {
+    books, maxPages, errorMessage,
+    nextPage, previousPage,
+    searchParameter, searchParameterValue, searchAvailableModifier,
+    setSearchParameter, setSearchParameterValue, modifyAvailableParameter
+  } = useSearchBooks()
+
+  return (
+    <div>BooksPage</div>
+  )
+}
