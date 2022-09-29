ed23693c5ee5d66a613aa5abdde4a6e7bb5c4282
Mauricio Gutiérrez <103621275+gutierrezdnf@users.noreply.github.com>
2022-09-29 07:37:03 -0600

    Removing .venv folder

--- tests/tests/conftest.py
+++ tests/tests/conftest.py
@@ -2,9 +2,9 @@ from pytest import fixture
 from selenium import webdriver
 
 
-@fixture(params=['firefox'], scope='class')
+@fixture(params=['chrome'], scope='class')
 def init_driver(request):
-    web_driver = webdriver.Chrome() if request.param == 'chrome' else webdriver.Firefox('/home/dicarlomtz/Downloads/geckodriver-v0.31.0-linux64/geckodriver')
+    web_driver = webdriver.Chrome() if request.param == 'chrome' else webdriver.Firefox()
     request.cls.driver = web_driver
     yield
     web_driver.close()
